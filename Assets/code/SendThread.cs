1acc252dabf92d662c2343c1be7280fc34e3032f
skatinsam <skatinsam2004@yahoo.com>
2014-05-01 10:33:01 -0700

    add sockets for client side

---
+++ Assets/code/SendThread.cs
@@ -0,0 +1,74 @@
+﻿// SWARCH
+
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+public class SendThread : MonoBehaviour 
+{
+	private NetworkStream nws;
+	private byte[] streamBuffer;
+	private byte byteBuffer;
+	private byte tempBuffer;
+	private Sockets socks;
+	private StreamWriter sw;
+	
+	private System.Object thisLock = new System.Object();
+	
+	public SendThread (NetworkStream nwsIn, Sockets inSocket )
+	{
+		nws = nwsIn;
+		
+		socks = inSocket;
+		sw = new StreamWriter(nws);
+		sw.AutoFlush = true;
+		
+		
+		
+	}
+	void Start()
+	{
+		
+	}
+	public void Service()
+	{
+		///*
+		while(socks.connected)
+		{ 
+			lock(thisLock)
+			{
+				
+				try
+				{ 
+					
+					
+					while(socks.sendQueue.Count > 0)
+					{
+						
+						string sendout = (string)socks.sendQueue.Dequeue();
+						
+						//if(sendout)
+						//sendoutSubstring(1,3); 
+						// if( starts with "start" )
+						//Thread.Sleep(100);
+						sw.WriteLine(sendout); //(toSend);
+						print ("\nSENT OUT(thread) ------>  " + sendout);
+						
+					}	
+				}
+				catch ( Exception ex )
+				{
+					print ( ex.Message + ": On SEND OUT (thread)" );
+				}	
+			}
+		}
+		//*/
+	}
+	
+	
+	
+}
---
+++ Assets/code/ThreadSock.cs
@@ -0,0 +1,85 @@
+﻿// SWARCH
+
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+public class ThreadSock : MonoBehaviour 
+{
+	private NetworkStream nws;
+	private byte[] streamBuffer;
+	private byte byteBuffer;
+	private byte tempBuffer;
+	private Sockets socks;
+	private StreamReader sr;
+	private System.Object thisLock = new System.Object();
+	
+	//********* COMPLETE THE FOLLOWING CODE
+	public ThreadSock (NetworkStream nwsIn, Sockets inSocket )
+	{
+		//?? nws = new NetworkStream(; //nwsIn;
+		nws = nwsIn;
+		
+		//?? socks = new Sockets();
+		socks = inSocket;
+		
+		sr = new StreamReader(nws);
+		
+		
+	}
+	public void Service ()
+	{	
+		//lock inside while loop
+		try
+		{
+			lock( thisLock)
+			{
+				
+				// Create an instance of StreamReader to read from a file. 
+				// The using statement also closes the StreamReader. 
+				//using (socks.sr) 
+				//{
+				// while(true)
+				//{
+				string line;
+				// Read and display lines from the file until the end of  
+				// the file is reached. 
+				while ((line = sr.ReadLine()) != null) 
+				{
+					
+					print("Received this <------ " + line);
+					
+					socks.recvBuffer.Enqueue(line);
+					
+				}
+				
+			}
+			
+			
+			//while(true)
+			//{
+			/*
+				if((byteBuffer = (byte)nws.ReadByte()) != -1 )
+				{
+					lock(socks.recvBuffer)
+					{
+
+						socks.recvBuffer.Enqueue(byteBuffer);
+					}
+				}
+              */
+			//	}
+			
+		}
+		catch ( Exception ex )
+		{
+			print ( ex.Message + " : ThreadSocket loop" );
+			
+		}
+		
+	}
+	
+}
